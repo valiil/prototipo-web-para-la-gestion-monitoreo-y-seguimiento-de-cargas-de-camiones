29c5c875197da4b51b28e7da61597acf3a2542b8
valiil <110740275+valiil@users.noreply.github.com>
2025-07-26 14:30:30 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 origins = [
     #"http://localhost.tiangolo.com",
     #"https://localhost.tiangolo.com",
-    "http://localhost"
+    "http://localhost",
+    "http://localhost:4200"
     #"http://localhost:8080",
 ]
 
